5065ceaeb75cb3d5f930ccd7192401481adf27e0
Kevin Eapen <kevineapen777@gmail.com>
2022-08-14 00:23:48 -0700

    Adding mentorship_eligibility table and output CSV.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -17,9 +17,9 @@ ORDER BY e.emp_no ASC;
 -- Remove duplicates from retirement_titles and create new table
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+    first_name,
+    last_name,
+    title
 INTO unique_titles
 FROM retirement_titles
 WHERE to_date = ('9999-01-01')
@@ -31,3 +31,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY title_total DESC;
+
+-- Create new table for employees eligible for mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
